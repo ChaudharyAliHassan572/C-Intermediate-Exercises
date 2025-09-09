1255c1a0a8caf4f26ff9af520e60a5ee36a682b5
Ali Hassan Chaudhary <Ali Hassan Chaudhary@DESKTOP-38I5CF2>
2025-09-09 09:02:24 +0500

    Add project files.

---
+++ C# Intermediate Exercises/Exercises/StopWatch.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace C__Intermediate_Exercises.Exercises
+{
+    internal class StopWatch
+    {
+        private DateTime StartTime { get; set; }
+        private DateTime StopTime { get; set; }
+        private bool _isRunning = false;
+
+        public void Start()
+        {
+            if (_isRunning)
+            {
+                throw new InvalidOperationException("The StopWatch is already Started!");
+            }
+            _isRunning = true;
+            StartTime = DateTime.Now;
+        }
+        public void Stop()
+        {
+            if (!_isRunning)
+            {
+                throw new InvalidOperationException("The StopWatch is not Started yet!");
+            }
+            _isRunning = false;
+            StopTime = DateTime.Now;
+        }
+        public TimeSpan Duration
+        {
+            get
+            {
+                if (_isRunning)
+                {
+                    throw new Exception("The StopWatch is still running!");
+                }
+                return StopTime - StartTime;
+            }
+        }
+    }
+}
---
+++ C# Intermediate Exercises/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using C__Intermediate_Exercises.Exercises;
+public class program
+{
+       public static void Main(string[] args)
+    {
+        // Exercise #1
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.Start ();
+        Console.WriteLine("The StopWatch is Started at: " + stopWatch.StartTime);
+        stopWatch.Stop();
+        Console.WriteLine("The StopWatch is Stopped at: " + stopWatch.StopTime);
+        Console.WriteLine(stopWatch.Duration);
+    }
+}
